502554e184d5a064a56eed274a4cf8d5fc6b2d02
marcprux <marc@glimpse.io>
2021-05-27 11:54:48 -0400

    build: restructure dependencies

--- Sources/GGViz/GGContext.swift
+++ Sources/GGViz/GGContext.swift
@@ -17,7 +17,11 @@ open class GGContext {
     let vg: JXValue
     let vge: JXValue
     let vgg: JXValue
+
     let vgg_compile: JXValue
+    let vg_parse: JXValue
+    let vg_View: JXValue
+    let vg_render: JXValue
 
     public init(ctx: JXContext = JXContext()) throws {
         self.ctx = ctx
@@ -50,7 +54,27 @@ open class GGContext {
         self.vge = try check(gv["vge"])
         self.vgg = try check(gv["vgg"])
 
+        self.vg_parse = try check(vg["parse"])
+
         self.vgg_compile = try check(vgg["compile"])
+
+        self.vg_View = try check(vg["View"])
+
+        self.vg_render = try check(ctx.eval(script: """
+            (function(vg, spec) {
+                var svg = "";
+                var view = new vg.View(vg.parse(spec), {renderer: 'none'});
+                view.run();
+                view.toSVG() // 
+                  .then(function(svgString) {
+                    // console.log("SVG", svgString);
+                    svg = svgString;
+                  })
+                  .catch(function(err) { console.error(err); });
+
+                return svg;
+            })
+            """))
     }
 
     deinit {
@@ -58,11 +82,25 @@ open class GGContext {
     }
 
     /// Compiles a spec
-    open func compileGrammar<T: Codable>(spec: T) throws -> JXValue {
+    open func compileGrammar<T: VizSpecMeta>(spec: VizSpec<T>) throws -> JXValue {
         try ctx.trying {
             try vgg_compile.call(withArguments: [ctx.encode(spec)])
         }
     }
+
+    open func parseViz(_ spec: JXValue) throws -> JXValue {
+        try ctx.trying {
+            vg_parse.call(withArguments: [spec])
+        }
+    }
+
+    open func renderViz(_ spec: JXValue) throws -> JXValue {
+        try ctx.trying {
+            vg_render.call(withArguments: [vg, spec])
+        }
+    }
+
+
 }
 
 
--- Tests/GGVizTests/GGVizTests.swift
+++ Tests/GGVizTests/GGVizTests.swift
@@ -19,6 +19,19 @@ final class GGVizTests: XCTestCase {
         dbg("compiled", value.toJSON(indent: 2))
         let compiled = try value.toDecodable(ofType: Bric.self)
 
+        let parsed = try ctx.parseViz(value)
+        dbg("parsed", parsed.toJSON(indent: 2))
+
+        let rendered = try ctx.renderViz(value)
+
+        XCTAssertEqual(rendered.stringValue, """
+            """)
+        
+//        measure { // measured [Time, seconds] average: 0.005, relative standard deviation: 24.014%, values: [0.007017, 0.005723, 0.005122, 0.007813, 0.004534, 0.004649, 0.004680, 0.004304, 0.004095, 0.003790]
+//            let value = try? ctx.compileGrammar(spec: spec)
+//            let _ = try? value?.toDecodable(ofType: Bric.self)
+//        }
+
         XCTAssertEqual(compiled, [
             "spec": [
               "$schema": "https://vega.github.io/schema/vega/v5.json",
