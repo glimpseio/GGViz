5d50ebae419336d985c75eb2ac90e665b011a5e7
marcprux <marc@glimpse.io>
2021-06-24 23:32:40 -0400

    feat: more DSL

---
+++ Sources/GGDSL/Fluent.swift
@@ -0,0 +1,162 @@
+
+
+/// A type that converts the properties of an encapsulated instance into dynamic builder DSL types. For example, the following will be equivalent:
+///
+/// ```swift
+/// var ob = Thing()
+/// ob.name = "ABC"
+/// ob.name("ABC")
+/// ```
+///
+/// In addition, properties that reach towards `OneOf` types will also have setters for each of the union types. For example:
+///
+/// ```swift
+/// struct Thing {
+///     var prop: OneOf2<String>.Or<Int>
+/// }
+///
+/// thing.prop = .v1("String")
+/// thing.prop = .v2(123)
+///
+/// thing.prop("ABC")
+/// thing.prop(123)
+/// ```
+///
+/// - Note: This type is compatible with `RawRepresentable`, but it does not conform to it. The encapsulated `rawValue` is not guaranteed to completely represent the underlying value.
+public protocol FluentContainer {
+    associatedtype RawValue
+    var rawValue: RawValue { get set }
+}
+
+/// A wrapper around an arbitrary instance that exposes fluent-style
+/// function properties for all of the writable key paths of the raw value.
+@frozen public struct Fluent<RawValue> : RawIsomorphism, FluentContainer {
+    public var rawValue: RawValue
+
+    public init(rawValue: RawValue) {
+        self.rawValue = rawValue
+    }
+}
+
+extension FluentContainer {
+    /// Fluent DSL setting function for all the mutable properties of an instance
+    @inlinable public subscript<U>(dynamicMember keyPath: WritableKeyPath<RawValue, U>) -> (U) -> (Self) {
+        { assigning(value: $0, to: keyPath, with: { $0 }) }
+    }
+
+    /// Fluent DSL setting function for all the mutable properties of a raw instance
+    @inlinable public subscript<Raw: RawInitializable, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Raw?>) -> (U) -> (Self) where Raw.RawValue == U {
+        { assigning(value: $0, to: keyPath, with: { Raw(rawValue: $0) }) }
+    }
+
+    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance
+    @inlinable public subscript<Choice: OneOfNType, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (U) -> (Self) where Choice.T1 == U {
+        { assigning(value: $0, to: keyPath, with: { .init($0) }) }
+    }
+
+    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value
+    @inlinable public subscript<Choice: OneOfNType, Raw: RawInitializable, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (U) -> (Self) where Choice.T1 == Raw, Raw.RawValue == U {
+        { assigning(value: $0, to: keyPath, with: { .init(Raw(rawValue: $0)) }) }
+    }
+
+    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value to another `OneOf` type
+    @inlinable public subscript<Choice: OneOf2Type, Raw: RawInitializable, Choice2: OneOfNType>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (Choice2.T1) -> (Self) where Choice.T1 == Raw, Raw.RawValue == Choice2 {
+        { assigning(value: $0, to: keyPath, with: { .init(Raw(rawValue: .init($0))) }) }
+    }
+
+    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value to another `OneOf` type
+    @inlinable public subscript<Choice1: OneOf2Type, Raw: RawInitializable, Choice2: OneOfNType, Choice3: OneOf2Type>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice1?>) -> (Choice3.T1) -> (Self) where Choice1.T1 == Raw, Raw.RawValue == Choice2, Choice2.T1 == Choice3 {
+        { assigning(value: $0, to: keyPath, with: { .init(Raw(rawValue: .init(.init($0)))) }) }
+    }
+
+    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value to another `OneOf` type
+    @inlinable public subscript<Choice1: OneOf2Type, Raw1: RawInitializable, Choice2: OneOfNType, Raw2: RawInitializable>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice1?>) -> (Choice1.T1.RawValue.T1.RawValue) -> (Self) where Choice1.T1 == Raw1, Raw1.RawValue == Choice2, Choice2.T1 == Raw2 {
+        { assigning(value: $0, to: keyPath, with: { .init(Raw1(rawValue: .init(Raw2(rawValue: $0)))) }) }
+    }
+
+
+    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance
+    @inlinable public subscript<Choice: OneOf2Type, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (U) -> (Self) where Choice.T2 == U {
+        { assigning(value: $0, to: keyPath, with: { .init($0) }) }
+    }
+
+    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value
+    @inlinable public subscript<Choice: OneOf2Type, Raw: RawInitializable, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (U) -> (Self) where Choice.T2 == Raw, Raw.RawValue == U {
+        { assigning(value: $0, to: keyPath, with: { .init(Raw(rawValue: $0)) }) }
+    }
+
+
+
+    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance
+    @inlinable public subscript<Choice: OneOf3Type, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (U) -> (Self) where Choice.T3 == U {
+        { assigning(value: $0, to: keyPath, with: { .init($0) }) }
+    }
+
+    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value
+    @inlinable public subscript<Choice: OneOf3Type, Raw: RawInitializable, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (U) -> (Self) where Choice.T3 == Raw, Raw.RawValue == U {
+        { assigning(value: $0, to: keyPath, with: { .init(Raw(rawValue: $0)) }) }
+    }
+
+
+
+    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance
+    @inlinable public subscript<Choice: OneOf4Type, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (U) -> (Self) where Choice.T4 == U {
+        { assigning(value: $0, to: keyPath, with: { .init($0) }) }
+    }
+
+
+    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value
+    @inlinable public subscript<Choice: OneOf4Type, Raw: RawInitializable, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (U) -> (Self) where Choice.T4 == Raw, Raw.RawValue == U {
+        { assigning(value: $0, to: keyPath, with: { .init(Raw(rawValue: $0)) }) }
+    }
+
+
+
+    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance
+    @inlinable public subscript<Choice: OneOf5Type, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (U) -> (Self) where Choice.T5 == U {
+        { assigning(value: $0, to: keyPath, with: { .init($0) }) }
+    }
+
+    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value
+    @inlinable public subscript<Choice: OneOf5Type, Raw: RawInitializable, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (U) -> (Self) where Choice.T5 == Raw, Raw.RawValue == U {
+        { assigning(value: $0, to: keyPath, with: { .init(Raw(rawValue: $0)) }) }
+    }
+
+
+
+    // these are deepest dynamic path we expose, explicitly to support easy initialization of constants like:
+    // Choice1: TimeUnitChoice = OneOf<TimeUnit>.Or<TimeUnitParams>
+    // Raw1: TimeUnit
+    // Choice2: TimeUnit.RawValue = OneOf<SingleTimeUnit>.Or<MultiTimeUnit>
+    // Raw2: SingleTimeUnit
+    // Choice3: SingleTimeUnit.RawValue = OneOf<LocalSingleTimeUnit>.Or<UtcSingleTimeUnit>
+    // Choice3.T1: LocalSingleTimeUnit { case year, quarter, month, … }
+
+    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value to another `OneOf` type
+    @inlinable public subscript<Choice1: OneOfNType, Raw1: RawInitializable, Choice2: OneOfNType, Raw2: RawInitializable, Choice3: OneOfNType>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice1?>) -> (Choice1.T1.RawValue.T1.RawValue.T1) -> (Self) where Choice1.T1 == Raw1, Raw1.RawValue == Choice2, Choice2.T1 == Raw2, Raw2.RawValue == Choice3 {
+        { assigning(value: $0, to: keyPath, with: { .init(Raw1(rawValue: .init(Raw2(rawValue: .init($0))))) }) }
+    }
+
+    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value to another `OneOf` type
+    @inlinable public subscript<Choice1: OneOfNType, Raw1: RawInitializable, Choice2: OneOfNType, Raw2: RawInitializable, Choice3: OneOf2Type>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice1?>) -> (Choice1.T1.RawValue.T1.RawValue.T2) -> (Self) where Choice1.T1 == Raw1, Raw1.RawValue == Choice2, Choice2.T1 == Raw2, Raw2.RawValue == Choice3 {
+        { assigning(value: $0, to: keyPath, with: { .init(Raw1(rawValue: .init(Raw2(rawValue: .init($0))))) }) }
+    }
+
+    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value to another `OneOf` type
+    @inlinable public subscript<Choice1: OneOfNType, Raw1: RawInitializable, Choice2: OneOf2Type, Raw2: RawInitializable, Choice3: OneOfNType>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice1?>) -> (Choice1.T1.RawValue.T2.RawValue.T1) -> (Self) where Choice1.T1 == Raw1, Raw1.RawValue == Choice2, Choice2.T2 == Raw2, Raw2.RawValue == Choice3 {
+        { assigning(value: $0, to: keyPath, with: { .init(Raw1(rawValue: .init(Raw2(rawValue: .init($0))))) }) }
+    }
+
+    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value to another `OneOf` type
+    @inlinable public subscript<Choice1: OneOfNType, Raw1: RawInitializable, Choice2: OneOf2Type, Raw2: RawInitializable, Choice3: OneOf2Type>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice1?>) -> (Choice1.T1.RawValue.T2.RawValue.T2) -> (Self) where Choice1.T1 == Raw1, Raw1.RawValue == Choice2, Choice2.T2 == Raw2, Raw2.RawValue == Choice3 {
+        { assigning(value: $0, to: keyPath, with: { .init(Raw1(rawValue: .init(Raw2(rawValue: .init($0))))) }) }
+    }
+
+
+    /// Assigns the given  value to the keyPath using the specific transform function
+    @usableFromInline func assigning<T, U>(value: T, to keyPath: WritableKeyPath<RawValue, U>, with transform: (T) throws -> (U)) rethrows -> Self {
+        var this = self
+        this.rawValue[keyPath: keyPath] = try transform(value)
+        return this
+    }
+}
--- Sources/GGDSL/GGDSL.swift
+++ Sources/GGDSL/GGDSL.swift
@@ -52,158 +52,6 @@ extension GG.FieldName : FieldNameRepresentable {
     public var fieldName: GG.FieldName { self }
 }
 
-/// A type that converts the properties of an encapsulated instance into dynamic builder DSL types. For example, the following will be equivalent:
-///
-/// ```swift
-/// var ob = Thing()
-/// ob.name = "ABC"
-/// ob.name("ABC")
-/// ```
-///
-/// In addition, properties that reach towards `OneOf` types will also have setters for each of the union types. For example:
-///
-/// ```swift
-/// struct Thing {
-///     var prop: OneOf2<String>.Or<Int>
-/// }
-///
-/// thing.prop = .v1("String")
-/// thing.prop = .v2(123)
-///
-/// thing.prop("ABC")
-/// thing.prop(123)
-/// ```
-///
-/// - Note: This type is compatible with `RawRepresentable`, but it does not conform to it. The encapsulated `rawValue` is not guaranteed to completely represent the underlying value.
-public protocol EncapsulatedPropertyRouter {
-    associatedtype RawValue
-    var rawValue: RawValue { get set }
-}
-
-extension EncapsulatedPropertyRouter {
-    /// Fluent DSL setting function for all the mutable properties of an instance
-    @inlinable public subscript<U>(dynamicMember keyPath: WritableKeyPath<RawValue, U>) -> (U) -> (Self) {
-        { assigning(value: $0, to: keyPath, with: { $0 }) }
-    }
-
-    /// Fluent DSL setting function for all the mutable properties of a raw instance
-    @inlinable public subscript<Raw: RawInitializable, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Raw?>) -> (U) -> (Self) where Raw.RawValue == U {
-        { assigning(value: $0, to: keyPath, with: { Raw(rawValue: $0) }) }
-    }
-
-    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance
-    @inlinable public subscript<Choice: OneOfNType, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (U) -> (Self) where Choice.T1 == U {
-        { assigning(value: $0, to: keyPath, with: { .init($0) }) }
-    }
-
-    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value
-    @inlinable public subscript<Choice: OneOfNType, Raw: RawInitializable, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (U) -> (Self) where Choice.T1 == Raw, Raw.RawValue == U {
-        { assigning(value: $0, to: keyPath, with: { .init(Raw(rawValue: $0)) }) }
-    }
-
-    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value to another `OneOf` type
-    @inlinable public subscript<Choice: OneOf2Type, Raw: RawInitializable, Choice2: OneOfNType>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (Choice2.T1) -> (Self) where Choice.T1 == Raw, Raw.RawValue == Choice2 {
-        { assigning(value: $0, to: keyPath, with: { .init(Raw(rawValue: .init($0))) }) }
-    }
-
-    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value to another `OneOf` type
-    @inlinable public subscript<Choice1: OneOf2Type, Raw: RawInitializable, Choice2: OneOfNType, Choice3: OneOf2Type>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice1?>) -> (Choice3.T1) -> (Self) where Choice1.T1 == Raw, Raw.RawValue == Choice2, Choice2.T1 == Choice3 {
-        { assigning(value: $0, to: keyPath, with: { .init(Raw(rawValue: .init(.init($0)))) }) }
-    }
-
-    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value to another `OneOf` type
-    @inlinable public subscript<Choice1: OneOf2Type, Raw1: RawInitializable, Choice2: OneOfNType, Raw2: RawInitializable>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice1?>) -> (Choice1.T1.RawValue.T1.RawValue) -> (Self) where Choice1.T1 == Raw1, Raw1.RawValue == Choice2, Choice2.T1 == Raw2 {
-        { assigning(value: $0, to: keyPath, with: { .init(Raw1(rawValue: .init(Raw2(rawValue: $0)))) }) }
-    }
-
-
-    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance
-    @inlinable public subscript<Choice: OneOf2Type, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (U) -> (Self) where Choice.T2 == U {
-        { assigning(value: $0, to: keyPath, with: { .init($0) }) }
-    }
-
-    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value
-    @inlinable public subscript<Choice: OneOf2Type, Raw: RawInitializable, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (U) -> (Self) where Choice.T2 == Raw, Raw.RawValue == U {
-        { assigning(value: $0, to: keyPath, with: { .init(Raw(rawValue: $0)) }) }
-    }
-
-
-
-    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance
-    @inlinable public subscript<Choice: OneOf3Type, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (U) -> (Self) where Choice.T3 == U {
-        { assigning(value: $0, to: keyPath, with: { .init($0) }) }
-    }
-
-    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value
-    @inlinable public subscript<Choice: OneOf3Type, Raw: RawInitializable, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (U) -> (Self) where Choice.T3 == Raw, Raw.RawValue == U {
-        { assigning(value: $0, to: keyPath, with: { .init(Raw(rawValue: $0)) }) }
-    }
-
-
-
-    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance
-    @inlinable public subscript<Choice: OneOf4Type, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (U) -> (Self) where Choice.T4 == U {
-        { assigning(value: $0, to: keyPath, with: { .init($0) }) }
-    }
-
-    
-    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value
-    @inlinable public subscript<Choice: OneOf4Type, Raw: RawInitializable, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (U) -> (Self) where Choice.T4 == Raw, Raw.RawValue == U {
-        { assigning(value: $0, to: keyPath, with: { .init(Raw(rawValue: $0)) }) }
-    }
-
-
-
-    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance
-    @inlinable public subscript<Choice: OneOf5Type, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (U) -> (Self) where Choice.T5 == U {
-        { assigning(value: $0, to: keyPath, with: { .init($0) }) }
-    }
-
-    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value
-    @inlinable public subscript<Choice: OneOf5Type, Raw: RawInitializable, U>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice?>) -> (U) -> (Self) where Choice.T5 == Raw, Raw.RawValue == U {
-        { assigning(value: $0, to: keyPath, with: { .init(Raw(rawValue: $0)) }) }
-    }
-
-
-
-    // these are deepest dynamic path we expose, explicitly to support easy initialization of constants like:
-    // Choice1: TimeUnitChoice = OneOf<TimeUnit>.Or<TimeUnitParams>
-    // Raw1: TimeUnit
-    // Choice2: TimeUnit.RawValue = OneOf<SingleTimeUnit>.Or<MultiTimeUnit>
-    // Raw2: SingleTimeUnit
-    // Choice3: SingleTimeUnit.RawValue = OneOf<LocalSingleTimeUnit>.Or<UtcSingleTimeUnit>
-    // Choice3.T1: LocalSingleTimeUnit { case year, quarter, month, … }
-
-    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value to another `OneOf` type
-    @inlinable public subscript<Choice1: OneOfNType, Raw1: RawInitializable, Choice2: OneOfNType, Raw2: RawInitializable, Choice3: OneOfNType>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice1?>) -> (Choice1.T1.RawValue.T1.RawValue.T1) -> (Self) where Choice1.T1 == Raw1, Raw1.RawValue == Choice2, Choice2.T1 == Raw2, Raw2.RawValue == Choice3 {
-        { assigning(value: $0, to: keyPath, with: { .init(Raw1(rawValue: .init(Raw2(rawValue: .init($0))))) }) }
-    }
-
-    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value to another `OneOf` type
-    @inlinable public subscript<Choice1: OneOfNType, Raw1: RawInitializable, Choice2: OneOfNType, Raw2: RawInitializable, Choice3: OneOf2Type>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice1?>) -> (Choice1.T1.RawValue.T1.RawValue.T2) -> (Self) where Choice1.T1 == Raw1, Raw1.RawValue == Choice2, Choice2.T1 == Raw2, Raw2.RawValue == Choice3 {
-        { assigning(value: $0, to: keyPath, with: { .init(Raw1(rawValue: .init(Raw2(rawValue: .init($0))))) }) }
-    }
-
-    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value to another `OneOf` type
-    @inlinable public subscript<Choice1: OneOfNType, Raw1: RawInitializable, Choice2: OneOf2Type, Raw2: RawInitializable, Choice3: OneOfNType>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice1?>) -> (Choice1.T1.RawValue.T2.RawValue.T1) -> (Self) where Choice1.T1 == Raw1, Raw1.RawValue == Choice2, Choice2.T2 == Raw2, Raw2.RawValue == Choice3 {
-        { assigning(value: $0, to: keyPath, with: { .init(Raw1(rawValue: .init(Raw2(rawValue: .init($0))))) }) }
-    }
-
-    /// Fluent DSL setting function for all the mutable `OneOf` properties of an instance that can be initialized with a raw value to another `OneOf` type
-    @inlinable public subscript<Choice1: OneOfNType, Raw1: RawInitializable, Choice2: OneOf2Type, Raw2: RawInitializable, Choice3: OneOf2Type>(dynamicMember keyPath: WritableKeyPath<RawValue, Choice1?>) -> (Choice1.T1.RawValue.T2.RawValue.T2) -> (Self) where Choice1.T1 == Raw1, Raw1.RawValue == Choice2, Choice2.T2 == Raw2, Raw2.RawValue == Choice3 {
-        { assigning(value: $0, to: keyPath, with: { .init(Raw1(rawValue: .init(Raw2(rawValue: .init($0))))) }) }
-    }
-
-
-    /// Assigns the given  value to the keyPath using the specific transform function
-    @usableFromInline func assigning<T, U>(value: T, to keyPath: WritableKeyPath<RawValue, U>, with transform: (T) throws -> (U)) rethrows -> Self {
-        var this = self
-        this.rawValue[keyPath: keyPath] = try transform(value)
-        return this
-    }
-}
-
-
 
 public protocol VizDSLType {
 }
@@ -259,7 +107,7 @@ public protocol VizTransformDefType : Pure {
 }
 
 @dynamicMemberLookup
-public struct VizTheme : VizSpecElementType, EncapsulatedPropertyRouter {
+public struct VizTheme : VizSpecElementType, FluentContainer {
     public var rawValue: GG.ConfigTheme
 
     public init(rawValue: RawValue = .init()) { self.rawValue = rawValue }
@@ -272,7 +120,7 @@ public struct VizTheme : VizSpecElementType, EncapsulatedPropertyRouter {
 
 
 @dynamicMemberLookup
-public struct VizGeoProjection : VizSpecElementType, EncapsulatedPropertyRouter, VizDSLType {
+public struct VizGeoProjection : VizSpecElementType, FluentContainer, VizDSLType {
     public var rawValue: GG.Projection
 
     public init(rawValue projection: GG.Projection) {
@@ -293,7 +141,7 @@ public struct VizGeoProjection : VizSpecElementType, EncapsulatedPropertyRouter,
 
 
 @dynamicMemberLookup
-public struct VizViewport : VizSpecElementType, EncapsulatedPropertyRouter, VizDSLType {
+public struct VizViewport : VizSpecElementType, FluentContainer, VizDSLType {
     public var rawValue: GG.ViewBackground
 
     public init(rawValue viewport: GG.ViewBackground) {
@@ -312,7 +160,7 @@ public struct VizViewport : VizSpecElementType, EncapsulatedPropertyRouter, VizD
 
 /// Literal data values to be included in a visualization
 @dynamicMemberLookup
-public struct DataValues : VizSpecElementType, EncapsulatedPropertyRouter, VizDSLType {
+public struct DataValues : VizSpecElementType, FluentContainer, VizDSLType {
     public var rawValue: GG.InlineDataset
 
     public init(rawValue dataSet: GG.InlineDataset) {
@@ -342,7 +190,7 @@ public struct DataValues : VizSpecElementType, EncapsulatedPropertyRouter, VizDS
 
 /// A reference to a give UR:L
 @dynamicMemberLookup
-public struct DataReference : VizSpecElementType, EncapsulatedPropertyRouter, VizDSLType {
+public struct DataReference : VizSpecElementType, FluentContainer, VizDSLType {
     public var rawValue: GG.UrlData
 
     public init(rawValue urlReference: GG.UrlData) {
@@ -367,7 +215,7 @@ public struct DataReference : VizSpecElementType, EncapsulatedPropertyRouter, Vi
 
 
 @dynamicMemberLookup
-public struct VizTransform<Def : VizTransformDefType> : VizSpecElementType, VizTransformType, EncapsulatedPropertyRouter {
+public struct VizTransform<Def : VizTransformDefType> : VizSpecElementType, VizTransformType, FluentContainer {
     public var rawValue: Def
     private let makeElements: () -> [VizLayerElementType]
 
@@ -751,7 +599,7 @@ public extension VizTransform where Def == GG.PivotTransform {
 
 /// A `Viz` encapsulates a top-level `VizSpec` layer and is used as the basis for the builder DSL.
 @dynamicMemberLookup
-public struct Viz<M: Pure> : VizLayerType, EncapsulatedPropertyRouter {
+public struct Viz<M: Pure> : VizLayerType, FluentContainer {
     public var rawValue: VizSpec<M>
 
     public init(rawValue: VizSpec<M> = VizSpec<M>()) {
@@ -783,7 +631,7 @@ extension Viz : CustomDebugStringConvertible {
 
 
 @dynamicMemberLookup
-public struct VizMark<Def : VizMarkDefType> : VizMarkType, VizDSLType, EncapsulatedPropertyRouter {
+public struct VizMark<Def : VizMarkDefType> : VizMarkType, VizDSLType, FluentContainer {
     public var markDef: Def
     public var encodings: GG.EncodingChannelMap = GG.EncodingChannelMap()
 
@@ -972,7 +820,7 @@ extension VizEncodeElementType {
 
 /// Scales map data values (numbers, dates, categories, etc.) to visual values (pixels, colors, sizes).
 @dynamicMemberLookup
-public struct VizScale : VizScaleEncodeElementType, VizDSLType, EncapsulatedPropertyRouter {
+public struct VizScale : VizScaleEncodeElementType, VizDSLType, FluentContainer {
     public var rawValue: GG.ScaleDef
 
     public init(rawValue scaleDef: GG.ScaleDef = GG.ScaleDef()) {
@@ -1006,7 +854,7 @@ public extension VizScale {
 }
 
 @dynamicMemberLookup
-public struct VizGuide<Def : VizGuideDefType> : VizDSLType, EncapsulatedPropertyRouter {
+public struct VizGuide<Def : VizGuideDefType> : VizDSLType, FluentContainer {
     public var rawValue: Def
 
     public init(rawValue guideDef: Def) {
@@ -1229,7 +1077,7 @@ public protocol VizEncodingChannelType : Pure, RawCodable {
 }
 
 @dynamicMemberLookup
-public struct VizEncode<Channel : VizEncodingChannelType, Def : Pure> : EncapsulatedPropertyRouter {
+public struct VizEncode<Channel : VizEncodingChannelType, Def : Pure> : FluentContainer {
     public var def: Def
     private let deriveChannel: (Def) -> (Channel)
     private let makeElements: () -> [VizEncodeElementType]
